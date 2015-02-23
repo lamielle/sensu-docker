aa1795bb6c8d151e5dbb419bab8bf7636b6690b8
Alan LaMielle <alan.lamielle+github@gmail.com>
2015-02-22 17:30:30 -0800

    Clean up and simplify influx extension handler

--- influx.rb
+++ influx.rb
@@ -3,54 +3,73 @@ require 'influxdb'
 
 # From https://github.com/lusis/sensu_influxdb_handler
 module Sensu::Extension
-
   class Influx < Handler
-
-    def name
-      'influx'
-    end
-
-    def description
-      'outputs metrics to InfluxDB'
-    end
-
+    # The post_init hook is called after the main event loop has started
+    # At this time EventMachine is available for interaction.
     def post_init
-      @influxdb = InfluxDB::Client.new settings['influx']['database'], :host => settings['influx']['host'], :port => settings['influx']['port'], :username => settings['influx']['user'], :password => settings['influx']['password']
+      server   = settings['influx']['host']
+      port     = settings['influx']['port']
+      username = settings['influx']['username']
+      password = settings['influx']['password']
+      database = settings['influx']['database']
+
+      @influxdb = InfluxDB::Client.new database,
+                    host: server,
+                    server: server,
+                    port: port,
+                    username: username,
+                    password: password
     end
 
+    # Must at a minimum define type and name. These are used by
+    # Sensu during extension initialization.
+    def definition
+      {
+        type: 'extension', # Always.
+        name: 'influx', # Usually just class name lower case.
+        mutator: 'ruby_hash'
+      }
+    end
+
+    # Simple accessor for the extension's name. You could put a different
+    # value here that is slightly more descriptive, or you could simply
+    # refer to the definition name.
+    def name
+      definition[:name]
+    end
+
+    # A simple, brief description of the extension's function.
+    def description
+      'Outputs metrics to InfluxDB'
+    end
+
+    # run() is passed a copy of the event_data hash
     def run(event)
       begin
-        event = Oj.load(event)
-        host = event[:client][:name]
-        series = event[:check][:name]
-        timestamp = event[:check][:issued]
-        duration = event[:check][:duration]
-        output = event[:check][:output]
-      rescue => e
-        @logger.error("InfluxDB: Error setting up event object - #{e.backtrace.to_s}")
-      end
-
-      begin
-        points = []
-        output.split(/\n/).each do |line|
-          @logger.debug("Parsing line: #{line}")
-          k,v,t = line.split(/\s+/)
-          v = v.match('\.').nil? ? Integer(v) : Float(v) rescue v.to_s
-          k.gsub!(/^.*#{@settings['influx']['strip_metric']}\.(.*$)/, '\1') if @settings['influx']['strip_metric']
-          points << {:time => t.to_f, :host => host, :metric => k, :value => v}
+        event[:check][:output].each_line do |metric|
+          @logger.debug("Parsing line: #{metric}")
+          m = metric.split
+          next unless m.count == 3
+          key = m[0].split('.', 2)[1]
+          next unless key
+          key.gsub!('.', '_')
+          value = m[1].to_f
+          mydata = { host: event[:client][:name], value: value,
+                     ip: event[:client][:address]
+                   }
+          @influxdb.write_point(key, mydata)
         end
       rescue => e
-        @logger.error("InfluxDB: Error parsing output lines - #{e.backtrace.to_s}")
-        @logger.error("InfluxDB: #{output}")
+        @logger.error("InfluxDB: Error posting event - #{e.backtrace.to_s}")
+        @logger.error("InfluxDB: #{event[:check][:output]}")
       end
 
-      begin
-        @influxdb.write_point(series, points, true)
-      rescue => e
-        @logger.error("InfluxDB: Error posting event - #{e.backtrace.to_s}")
-      end
-      yield("InfluxDB: Handler finished", 0)
+      yield '', 0
     end
 
+    # Called when Sensu begins to shutdown.
+    def stop
+      yield
+    end
   end
 end
