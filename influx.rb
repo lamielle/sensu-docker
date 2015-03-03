23278a40fceddff88531747e38a5565d9898ab16
Alan LaMielle <alan.lamielle+github@gmail.com>
2015-03-03 09:31:13 -0800

    Support key/value pairs in additional metric fields
    For fields beyond the third, we now parse key=value from each and insert
    as additional columns/values into the DB.

--- influx.rb
+++ influx.rb
@@ -7,18 +7,20 @@ module Sensu::Extension
     # The post_init hook is called after the main event loop has started
     # At this time EventMachine is available for interaction.
     def post_init
-      server   = settings['influx']['host']
-      port     = settings['influx']['port']
-      username = settings['influx']['username']
-      password = settings['influx']['password']
-      database = settings['influx']['database']
+      server         = settings['influx']['host']
+      port           = settings['influx']['port']
+      username       = settings['influx']['username']
+      password       = settings['influx']['password']
+      database       = settings['influx']['database']
+      time_precision = 's'
 
       @influxdb = InfluxDB::Client.new database,
                     host: server,
                     server: server,
                     port: port,
                     username: username,
-                    password: password
+                    password: password,
+                    time_precision: time_precision
     end
 
     # Must at a minimum define type and name. These are used by
@@ -46,18 +48,38 @@ module Sensu::Extension
     # run() is passed a copy of the event_data hash
     def run(event)
       begin
+        # Process each line as an individual data point.
+        # We expect lines to look like the following:
+        #
+        # series.name value timestamp meta1=v1 meta2=v2
+        #
+        # The first component of the series name will be dropped.
+        # The remaining '.' characters in the series name will be converted to '_'.
+        # The timestamp is unix timestamp (seconds).
+        # The metadata key/value pairs are optional.
         event[:check][:output].each_line do |metric|
           @logger.debug("Parsing line: #{metric}")
-          m = metric.split
-          next unless m.count == 3
-          key = m[0].split('.', 2)[1]
+          fields = metric.split
+          next unless fields.count >= 3
+
+          key = fields[0].split('.', 2)[1]
+          value = fields[1].to_f
+          time = fields[2].to_i
+          metadata = fields[3..fields.size]
           next unless key
+
           key.gsub!('.', '_')
-          value = m[1].to_f
-          mydata = { host: event[:client][:name], value: value,
-                     ip: event[:client][:address]
-                   }
-          @influxdb.write_point(key, mydata)
+          data = {
+                   value: value,
+                   time:  time,
+                   host:  event[:client][:name],
+                   ip:    event[:client][:address],
+                 }
+
+          metadata = Hash[fields[3..fields.size].map {|kv| kv.split('=')}]
+
+          @logger.debug("Inserting data=#{data} metadata=#{metadata} merged=#{data.merge(metadata)}")
+          @influxdb.write_point(key, data.merge(metadata))
         end
       rescue => e
         @logger.error("InfluxDB: Error posting event - #{e.backtrace.to_s}")
